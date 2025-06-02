a0c74eb8e72b4e86f517f189ea628a12485060e8
Fabian <fherlitz@outlook.com>
2025-06-02 19:13:23 +0200

    folder changes

--- yunni/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable static image optimization
-  images: {
-    domains: ['www.youtube.com', 'www.youtube-nocookie.com'],
-  },
-};
-
-export default nextConfig;
